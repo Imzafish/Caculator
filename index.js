34c1b67d9b0c35cfdf7ab166bc553cd63167f371
Imzafish <129023919+Imzafish@users.noreply.github.com>
2024-04-17 14:17:05 +1000

    Super close to finishing!

--- index.js
+++ index.js
@@ -1,34 +1,27 @@
-const prompt = require('prompt-sync')();
-var number1 = prompt('Number_1: ');
-var number2 = prompt('Number_2: ');
-var operation_type = prompt('Operation?:')
-operation_type.toLowerCase();
-function add(Num_1,Num_2){
-    return Num_1+Num_2
+//The brains for website
+function add(){
+    x = document.getElementById("num1").value;
+    y = document.getElementById("num2").value;
+    x = parseInt(x)
+    y = parseInt(y)
+    ans=x+y;
+    document.getElementById("ans_box").value = ans;
 }
-function multiply(Num_1,Num_2){
-    return Num_1*Num_2
+function multiply(){
+    x = document.getElementById("num1").value;
+    y = document.getElementById("num2").value;
+    ans=x*y;
+    document.getElementById("ans_box").value = ans;
 }
-function minus(Num_1,Num_2){
-    return Num_1-Num_2
+function minus(){
+    x = document.getElementById("num1").value;
+    y = document.getElementById("num2").value;
+    ans=x-y;
+    document.getElementById("ans_box").value = ans;
 }
-function divide(Num_1,Num_2){
-    return Num_1/Num_2
-}
-
-
-if (operation_type==="add"){
-    let ans = add(Number(number1),Number(number2))
-    console.log(ans);
-}else if (operation_type==="multiply"){
-    let ans = multiply(Number(number1),Number(number2))
-    console.log(ans);
-}else if (operation_type==="minus"){
-    let ans = minus(Number(number1),Number(number2))
-    console.log(ans);
-}else if (operation_type==="divide"){
-    let ans = divide(Number(number1),Number(number2))
-    console.log(ans);
-}else{
-    console.log("Error, this operation type is not supported.");
+function divide(){
+    x = document.getElementById("num1").value;
+    y = document.getElementById("num2").value;
+    ans=x/y;
+    document.getElementById("ans_box").value = ans;
 }
